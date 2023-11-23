00e1858ae84628deb5ab1c18d756db5e39374023
Nicolas Romero <nare.rare.fire@gmail.com>
2023-11-22 23:43:54 -0300

    changes

--- Assets/Water.cs
+++ Assets/Water.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+    public bool randomStart = false;
+
     public float heigth = 1f;
     public float speed = 10.0f;
 
     private Vector3 startPos;
-         
+    private float offsetTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+
+        if (randomStart)
+        {
+            offsetTime = Random.Range(0, 360f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        this.transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time * speed) * heigth, 0);
+        this.transform.position = startPos + new Vector3(0, Mathf.Sin((Time.time + offsetTime) * speed) * heigth, 0);
     }
 }
