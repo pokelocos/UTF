b86669fba0bef54c95f1efad60822b24addf2c92
Nicolas Romero <nare.rare.fire@gmail.com>
2023-12-01 10:06:40 -0300

    changes

--- Assets/PlayerData.cs
+++ Assets/PlayerData.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    public int life = 5;
+    public int maxLife = 5;
+
+    public int coins = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Load();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
-        
+        Save();
     }
+
+    public void Load()
+    {
+        life = PlayerPrefs.GetInt("life");
+        coins = PlayerPrefs.GetInt("coins");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("life", life);
+        PlayerPrefs.SetInt("coins", coins);
+    }
+}
+
+[CreateAssetMenu(fileName = "new SO", menuName = "UFT/Create SO")]
+public class CardSO : ScriptableObject
+{
+    public string cardName;
+    public int cardCost;
+    public int cardAttack;
+    public int cardHealth;
+    public Sprite cardSprite;
 }
